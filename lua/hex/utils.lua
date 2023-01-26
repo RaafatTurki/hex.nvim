3498657013f2eb938e9ee96712b88b8f21cae8df
Armin Veres <armin.veres@hotmail.com>
2023-01-26 20:21:36 +0100

    Fixed error on missing 'xxd' executable
    The plugin was giving me an error of a missing `xxd`, which was incorrect,
    as it was installed.
    Adjusted plugin source code to only return, if exit code is false.

--- lua/hex/utils.lua
+++ lua/hex/utils.lua
@@ -3,23 +3,23 @@ local M = {}
 function M.drop_undo_history()
   local undolevels = vim.o.undolevels
   vim.o.undolevels = -1
-  vim.cmd [[exe "normal a \<BS>\<Esc>"]]
+  vim.cmd([[exe "normal a \<BS>\<Esc>"]])
   vim.o.undolevels = undolevels
 end
 
 function M.dump_to_hex(hex_dump_cmd)
   vim.b.hex = true
-  vim.cmd([[%! ]]..hex_dump_cmd)
+  vim.cmd([[%! ]] .. hex_dump_cmd)
   vim.b.hex_ft = vim.bo.ft
-  vim.bo.ft = 'xxd'
+  vim.bo.ft = "xxd"
   M.drop_undo_history()
   -- FIXME: this can be problematic
-  vim.cmd [[LspStop]]
+  vim.cmd([[LspStop]])
   vim.bo.mod = false
 end
 
 function M.assemble_from_hex(hex_assemble_cmd)
-  vim.cmd([[%! ]]..hex_assemble_cmd)
+  vim.cmd([[%! ]] .. hex_assemble_cmd)
   vim.bo.ft = vim.b.hex_ft
   M.drop_undo_history()
   vim.bo.mod = false
@@ -28,23 +28,23 @@ end
 
 function M.begin_patch_from_hex(hex_assemble_cmd)
   vim.b.hex_cur_pos = vim.fn.getcurpos()
-  vim.cmd([[%! ]]..hex_assemble_cmd)
+  vim.cmd([[%! ]] .. hex_assemble_cmd)
 end
 
 function M.finish_patch_from_hex(hex_dump_cmd)
-  vim.cmd([[%! ]]..hex_dump_cmd)
-  vim.fn.setpos('.', vim.b.hex_cur_pos)
+  vim.cmd([[%! ]] .. hex_dump_cmd)
+  vim.fn.setpos(".", vim.b.hex_cur_pos)
   vim.bo.mod = true
 end
 
 function M.is_program_on_path(program)
-  local exit_code = os.execute('which '..program)
-  
-  if (exit_code == 0) then
+  local exit_code = os.execute("which " .. program)
+  if exit_code then
     return true
+  else
+    vim.notify(program .. " is not installed on this system, aborting!", vim.log.levels.ERROR)
+    return false
   end
-  vim.notify(program .. " is not installed on this system, aborting!", vim.log.levels.ERROR)
-  return false
 end
 
 return M
